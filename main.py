a9788e4e24352ae2b0bcc9300e351759e8372887
Oplayer1337 <44051933+Oplayer1337@users.noreply.github.com>
2022-04-14 21:37:55 +0500

    sell_cm полностью переписана

--- main.py
+++ main.py
@@ -116,8 +116,13 @@ def storage_model(m, ):
         del_cache1(m)
         return
 
-    st[d.get('Model')[int(get_cache(m)) - 1]] = {}
-    json_save_st()
+    try:
+        if st[d.get('Model')[int(get_cache(m)) - 1]] is dict:
+            pass
+    except KeyError:
+        st[d.get('Model')[int(get_cache(m)) - 1]] = {}
+        json_save_st()
+
     line = d.get('Flavours')
 
     msg = bot.reply_to(m, 'введите номер вкуса\n' + genlist(line))
@@ -134,8 +139,12 @@ def storage_flavours(m, ):
         del_cache1(m)
         return
 
-    st[d.get('Model')[int(get_cache(m)) - 1]][d.get('Flavours')[int(get_cache1(m)) - 1]] = 0
-    json_save_st()
+    try:
+        if st[d.get('Model')[int(get_cache(m)) - 1]][d.get('Flavours')[int(get_cache1(m)) - 1]] is dict:
+            pass
+    except KeyError:
+        st[d.get('Model')[int(get_cache(m)) - 1]][d.get('Flavours')[int(get_cache1(m)) - 1]] = 0
+        json_save_st()
 
     msg = bot.reply_to(m, 'Введите колличество взятых одноразок:')
     bot.register_next_step_handler(msg, storage_amount)
@@ -236,7 +245,7 @@ def amount(m, ):
         return
 
     if st[Model][Flavour] == int(get_cache(m)):
-        del st[Model]
+        del st[Model][Flavour]
         json_save_st()
     elif st[Model][Flavour] < int(get_cache(m)):
         bot.reply_to(m, 'Вы просите больше, чем есть на складе!')
@@ -264,8 +273,6 @@ def amount(m, ):
 @bot.message_handler(commands=["sell"])
 def sell_cb(m, ):
     del_model(m)
-    del_dict(m)
-    create_dict(m)
 
     line = list(hd[get_user_id(m)].keys())
 
@@ -279,11 +286,7 @@ def sell_cb(m, ):
     #     for j in range(2, len(c) - 1):
     #         s.append(c[j])
     # s = list(set(s))
-    # for i in range(len(s)):
-    #     if s[i] in str(d.get('Model')):
-    #         edit_dict(m, s[i], 'Model')
-    #     if s[i] in str(d.get('Flavours')):
-    #         edit_dict(m, s[i], 'Flavours')
+
     msg = bot.reply_to(m, 'Введите название модели: \n' + gensell(line))
 
     create_model(m)
@@ -295,64 +298,71 @@ def s_model(m, ):
     create_cache(m)
     edit_cache(m, m.text)
 
-    if (get_cache(m).isdigit() is False) or int(get_cache(m)) > len(get_dict(m).get('Model')) or int(get_cache(m)) == 0:
+    if (get_cache(m).isdigit() is False) or int(get_cache(m)) > len(list(hd[get_user_id(m)].keys())) or int(get_cache(m)) == 0:
         bot.reply_to(m, 'Неправильный номер модели')
         del_cache(m)
         del_model(m)
         return
 
-    edit_model_sell(m, int(get_cache(m)) - 1, 'Model')
-    del_cache(m)
-
-    line = get_dict(m).get('Flavours')
+    line = list(hd[get_user_id(m)].keys())
+    edit_cache(m, line[int(get_cache(m)) - 1])
+    edit_model(m,get_cache(m))
+    line = list(hd[get_user_id(m)][get_cache(m)].keys())
     msg = bot.reply_to(m, 'Введите номер вкуса:\n' + gensell(line))
     bot.register_next_step_handler(msg, s_flavours)
 
 
 def s_flavours(m, ):
-    global d
-    create_cache(m)
-    edit_cache(m, m.text)
-
-    if (get_cache(m).isdigit() is False) or (int(get_cache(m)) > len(get_dict(m).get('Flavours'))):
+    create_cache1(m)
+    edit_cache1(m, m.text)
+    if (get_cache1(m).isdigit() is False) or int(get_cache1(m)) > len(list(hd[get_user_id(m)][get_cache(m)].keys())) or int(get_cache1(m)) == 0:
         bot.reply_to(m, 'Неправильный номер вкуса')
         del_cache(m)
         del_model(m)
         return
 
-    edit_model_sell(m, int(get_cache(m)) - 1, 'Flavours')
-
-    del_cache(m)
+    line = list(hd[get_user_id(m)][get_cache(m)].keys())
+    edit_cache1(m, line[int(get_cache1(m)) - 1])
+    edit_model(m, get_cache1(m))
     msg = bot.reply_to(m, 'Введите колличество проданных одноразок:')
     bot.register_next_step_handler(msg, s_amount)
 
 
 def s_amount(m, ):
-    create_cache(m)
+    Model = get_cache(m)
+    Flavour = get_cache1(m)
     edit_cache(m, m.text)
-    del_dict(m)
-    if get_cache(m).isdigit() is False:
+
+    if get_cache(m).isdigit() is False or int(get_cache(m)) == 0:
         bot.reply_to(m, 'Неправильное число:')
         del_cache(m)
         del_model(m)
         return
 
-    user_id_ = 'hands_' + get_user_id(m) + '_'
-    user_id_model = user_id_ + get_model(m)
-    importlib.reload(id)
-    if search(user_id_model) == 0:
-        bot.reply_to(m, 'У вас нет этой модели с таким вкусом')
-        return
-
-    if int(getattr(id, user_id_model)) == int(get_cache(m)):
-        bot.reply_to(m, 'Вы  продали все одноразки этой модели с рук! Сколько вы получили?')
-        delite(user_id_model)
-        reload(id)
-    elif int(getattr(id, user_id_model)) < int(get_cache(m)):
-        bot.reply_to(m, 'У вас нет столько одноразок этой модели')
-        return
+    if int(get_cache(m)) > hd[get_user_id(m)][Model][Flavour]:
+        bot.reply_to(m, 'У вас нет столько на руках!')
+    elif int(get_cache(m)) == hd[get_user_id(m)][Model][Flavour]:
+        del hd[get_user_id(m)][Model][Flavour]
+        json_save_hd()
     else:
-        add(user_id_model, -int(get_cache(m)))
+        hd[get_user_id(m)][Model][Flavour] -= int(get_cache(m))
+        json_save_hd()
+    # user_id_ = 'hands_' + get_user_id(m) + '_'
+    # user_id_model = user_id_ + get_model(m)
+    # importlib.reload(id)
+    # if search(user_id_model) == 0:
+    #     bot.reply_to(m, 'У вас нет этой модели с таким вкусом')
+    #     return
+    #
+    # if int(getattr(id, user_id_model)) == int(get_cache(m)):
+    #     bot.reply_to(m, 'Вы  продали все одноразки этой модели с рук! Сколько вы получили?')
+    #     delite(user_id_model)
+    #     reload(id)
+    # elif int(getattr(id, user_id_model)) < int(get_cache(m)):
+    #     bot.reply_to(m, 'У вас нет столько одноразок этой модели')
+    #     return
+    # else:
+    #     add(user_id_model, -int(get_cache(m)))
     msg = bot.reply_to(m, 'Сколько вы получили с ' + get_cache(m) + ' одноразок этой модели?')
 
     user_id_sell = 'sell_' + get_user_id(m) + '_'
